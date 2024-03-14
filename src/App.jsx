ff97b4a11934738519cdcc979bc161b4be2a51a4
AngelLaines <angelmoreno755@gmail.com>
2024-03-13 17:10:26 -0700

    asdf

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { useEffect, useState } from 'react'
 function App() {
   const [data,setData] = useState('')
   useEffect(()=>{
-    fetch('https://localhost:44382/api/Printer').then(res=>res.json()).then(res=>{
+    fetch('https://localhost:4300/api/Printer').then(res=>res.json()).then(res=>{
       console.log(res);
       setData(res)
     })
